a1aee864e71d6dc80f0516292b0afcea47bd3451
Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
2020-12-28 03:22:03 +0530

    gallery initiaed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect, withRouter } from "react-router-dom";
 import Event from './Container/Event/Event';
+import Gallery from './Container/Gallery/Gallery';
 import Home from './Container/Home/Home';
 
 class App extends React.Component{
