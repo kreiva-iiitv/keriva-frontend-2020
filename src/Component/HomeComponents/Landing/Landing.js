76f14dbc6fb8e5b0296c8db837a866e6640f3e4d
sidharth <201952234@iiitvadodara.ac.in>
2021-01-06 03:26:12 +0530

    Fix Homepage Landing component issue resolved

--- src/Component/HomeComponents/Landing/Landing.js
+++ src/Component/HomeComponents/Landing/Landing.js
@@ -12,12 +12,14 @@ class Landing extends Component {
             
             <div className="landing">
 
-                <img className="wave" src={wave} />
+<div>
+            <img className="wave" src={wave} />
+           </div>
 
-               <div className="parent">
-                   <p  className="kreiva">KREIVA</p>
-                   <p className="kreiva a">2020</p>
-               </div>
+           <div className="parent">
+               <span  className="kreiva">KREIVA</span>
+               <span className="kreiva a">2020</span>
+           </div>
 
                <div className="virtual">
                    <p>A Virtual Fest</p>
