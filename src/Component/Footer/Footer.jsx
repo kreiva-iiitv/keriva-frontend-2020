fc8b288179e93f012f0a2a7ed1cde3ec5abf675d
Shivam <54373797+shivam0110@users.noreply.github.com>
2021-01-06 10:30:26 +0530

    footer updated (#11)
    * footer updated
    
    * Update footer
    
    * Updated Footer
    
    * changes in design of footer
    
    * final final design changes
    
    * hr border radius added
    
    * font family changed
    
    * add a tag
    
    * added hover effect on a tag
    
    Co-authored-by: = <=>

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
 
+import "./footcss.css";
+import inslogo from "./inslogo.png";
+import dotlogo from "./dot.png";
+import cclogo from "./inslogo.png";
+
 const Footer = () => {
   return (
     <footer>
-      Footer
+      <div className="inname">
+        <div>
+            <img src = {inslogo} alt="iiitv-logo" className="inslog"></img>
+        </div>
+        <div className="name">
+          Indian Institute of Information Technology Vadodara
+        </div>
+      </div>
+
+      <div className="linkcon">
+        <div className="footerlin">
+        <div className="linkna">
+          LINKS
+          <hr className="underline"></hr>
+        </div>
+        <div className="links">
+          <a href="" className="link">Home</a>
+          <a href="" className="link">Events</a>
+          <a href="" className="link">Members</a>
+          <a href="" className="link">Institute Site</a>
+        </div>
+        </div>
+        <div className="footerlin">
+        <div className="conna">
+          CONTACT
+          <hr className="underline"></hr>
+        </div>
+        <div className="contact">
+          <p className="clink">Email:</p>
+          <p className="clink">Phone:</p>
+          <p className="clink">Address:</p>
+        </div>
+        </div>
+      </div>
+      <hr className="lastline"></hr>
+
+      <div className="dev"> 
+        <div className="dot">
+          <p className="p-dev">Designed by Dot</p>
+          <img src = {dotlogo} alt="dot-logo" className="dotlogo"></img>
+        </div>
+        <div className="cc">
+          <p className="p-dev">Developed by Coding Club</p>
+          <img src = {cclogo} alt="cc-logo" className="dotlogo"></img>
+        </div>
+      </div>
     </footer>
   );
 }
