6789ff95c6de3ab1bcf5eb713656f62ec0ffa9db
= <=>
2021-01-07 10:40:03 +0530

    removed extra comments, simplified a code block

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -12,28 +12,21 @@ import cclogo from "../../Assets/Images/inslogo.png";
 
 const Footer = () => {
  
-  // var toogle = true;
   const link_toogle = () => {
     var element = document.getElementById("cyka");
 
-  if (element.classList) { 
-    element.classList.toggle("link-display");
-  } else {
-    var classes = element.className.split(" ");
-    var i = classes.indexOf("link-display");
+    if (element.classList) { 
+      element.classList.toggle("link-display");
+    } else {
+      var classes = element.className.split(" ");
+      var i = classes.indexOf("link-display");
 
-    if (i >= 0) 
-      classes.splice(i, 1);
-    else 
-      classes.push("link-display");
-      element.className = classes.join(" "); 
-  }
-
-    // let element = document.getElementById('cyka')
-    // ReactDOM.findDOMNode(element).style.display='block'    
-
-    // toogle =  toogle? false : true ;
-    // console.log(toogle);
+      if (i >= 0) 
+        classes.splice(i, 1);
+      else 
+        classes.push("link-display");
+        element.className = classes.join(" "); 
+    }
   };
 
   return (
@@ -60,10 +53,10 @@ const Footer = () => {
             </div>
           </div>
           <div className="links" id="cyka">
-            <a href="" className="link"  style={{display: 'block'}}>Home</a>
-            <a href="" className="link"  style={{display: 'block'}}>Events</a>
-            <a href="" className="link"  style={{display: 'block'}}>Members</a>
-            <a href="" className="link"  style={{display: 'block'}}>Institute Site</a>
+            <a href="" className="link">Home</a>
+            <a href="" className="link">Events</a>
+            <a href="" className="link">Members</a>
+            <a href="" className="link">Institute Site</a>
           </div>
         </div>
 
@@ -73,9 +66,9 @@ const Footer = () => {
           <hr className="underline"></hr>
         </div>
         <div className="contact">
-          <p className="clink">Email: <p className="con-details">cerebro@iiitvadodara.ac.in</p></p>
-          <p className="clink">Phone: <p className="con-details">+915525252536  +91242526252652  +9135252252255</p></p>
-          <p className="clink">Address: <p className="con-details">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p></p>
+          <div className="clink">Email: <div className="con-details">cerebro@iiitvadodara.ac.in</div></div>
+          <div className="clink">Phone: <div className="con-details">+915525252536  +91242526252652  +9135252252255</div></div>
+          <div className="clink">Address: <div className="con-details">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div></div>
         </div>
         </div>
       </div>
