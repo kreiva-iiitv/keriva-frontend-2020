7cb142478634f084f66f5524d524dc5a0ba872cf
Aman <archanaamanraj@gmail.com>
2021-01-10 03:22:08 +0530

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect, withRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Event from './Container/Event/Event';
 import Home from './Container/Home';
 
-class App extends React.Component{
-
-  state = {
-
-  };
-  
-  render(){
-    return(
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact
-            render={props => (
-              <Home />
-            )}
-          />
-          <Route path="/event" exact
-            render={props => (
-              <Event />
-            )}
-          />
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact
+          render={props => (
+            <Home />
+          )}
+        />
+        <Route path="/event" exact
+          render={props => (
+            <Event />
+          )}
+        />
       </Switch>
     </BrowserRouter>
-    );
-  };
+  );
 }
+
 export default App;
