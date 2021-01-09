ac66cbcbb317d570bda46b5fc208e751b755628e
Aditya Prakash <42460356+pAditya198@users.noreply.github.com>
2021-01-09 23:09:06 +0530

    Revert "Landing Fixed (#26)"
    This reverts commit 559e5465fbf8fdc99e5ace269b31df0544eaf1bc.

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,35 +1,21 @@
-import React from "react";
-import logo from "../../Assets/Images/logo.png";
+import React from 'react';
+import logo from '../../Assets/Images/logo.png'
 
-import "./Header.css";
+import classes from './Header.css';
 
 const Header = () => {
   return (
-    <header>
-      <div className="tabs">
-        <div className="tabs__logo__tab active">
-          <a>HOME</a>
+    <header className="tabs">
+      <a className="tab">HOME</a>
+      <a className="tab">EVENTS</a>
+      <div>
+        <img className="logo" src={logo} />
+        <p className="date">28th Dec 2020 - 10th Jan 2021</p>
         </div>
-
-        <div className="tabs__logo__tab ">
-          <a>EVENTS</a>
-        </div>
-
-        <div>
-          <img className="tabs__logo" src={logo} />
-          <p className="tabs__logo--date">28th Dec 2020 - 10th Jan 2021</p>
-        </div>
-
-        <div className="tabs__logo__tab">
-          <a>MEMBERS</a>
-        </div>
-
-        <div className="tabs__logo__tab">
-          <a>BROUCHURE</a>
-        </div>
-      </div>
+      <a className="tab">MEMBERS</a>
+      <a className="tab">BROUCHURE</a>
     </header>
   );
-};
+}
 
 export default Header;
---
+++ src/Container/Home/Home.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'; 
+
+import Landing from '../../Component/HomeComponents/Landing/Landing';
+import Footer from '../../Component/Footer/Footer';
+import Header from '../../Component/Header/Header';
+import About from '../../Component/HomeComponents/About/About';
+import classes from './Home.css';
+import Gallery from '../Gallery/Gallery';
+
+class Home extends Component {
+    render() {
+        return(
+            
+		    <React.Fragment>
+		      <Header />
+		      	<Landing/>
+				 <About/>
+				 <Gallery/> 	
+		      <Footer />
+		    </React.Fragment>
+            
+        )
+    }
+}
+
+export default Home;
