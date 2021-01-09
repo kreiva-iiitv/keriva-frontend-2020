aee4bcbd3729c7f74164fb68a241058f0a7e704c
Aditya Prakash <p.aditya.198@gmail.com>
2021-01-09 22:34:19 +0530

    landing page fixed

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,38 +1,35 @@
-import React from 'react';
-import logo from '../../Assets/Images/logo.png'
+import React from "react";
+import logo from "../../Assets/Images/logo.png";
 
-import classes from './Header.css';
+import "./Header.css";
 
 const Header = () => {
   return (
-    <header >
+    <header>
       <div className="tabs">
-
-        <div className="tab home">
-          <a >HOME</a>
+        <div className="tabs__logo__tab active">
+          <a>HOME</a>
         </div>
 
-        <div className="tab ">
-          <a >EVENTS</a>
+        <div className="tabs__logo__tab ">
+          <a>EVENTS</a>
         </div>
-     
+
         <div>
-          <img  className="logo" src={logo} />
-          <p className="date">28th Dec 2020 - 10th Jan 2021</p>
+          <img className="tabs__logo" src={logo} />
+          <p className="tabs__logo--date">28th Dec 2020 - 10th Jan 2021</p>
         </div>
 
-        <div className="tab">
-          <a >MEMBERS</a>
+        <div className="tabs__logo__tab">
+          <a>MEMBERS</a>
         </div>
-     
-        <div className="tab">
+
+        <div className="tabs__logo__tab">
           <a>BROUCHURE</a>
         </div>
-     
       </div>
-     
     </header>
   );
-}
+};
 
 export default Header;
