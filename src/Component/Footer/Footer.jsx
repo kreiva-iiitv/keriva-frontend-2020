c1b6285a7857ab21b58983b62558a9712d10748a
Aman <archanaamanraj@gmail.com>
2021-01-09 22:59:20 +0530

    mobile footer container css fix
    Footer css file name fix

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import "./footcss.css";
 import 'font-awesome/css/font-awesome.min.css';
+import "./Footer.css";
 
-//import images
+// import images
 import inslogo from "../../Assets/Images/inslogo.png";
 import dotlogo from "../../Assets/Images/dot.png";
 import cclogo from "../../Assets/Images/inslogo.png";
 
 
 const Footer = () => {
- 
+
   const link_toogle = () => {
     var element = document.getElementById("cyka");
 
-    if (element.classList) { 
+    if (element.classList) {
       element.classList.toggle("link-display");
     } else {
       var classes = element.className.split(" ");
       var i = classes.indexOf("link-display");
 
-      if (i >= 0) 
+      if (i >= 0)
         classes.splice(i, 1);
-      else 
+      else
         classes.push("link-display");
-        element.className = classes.join(" "); 
+      element.className = classes.join(" ");
     }
   };
 
@@ -33,7 +32,7 @@ const Footer = () => {
     <footer>
       <div className="inname">
         <div>
-            <img src = {inslogo} alt="iiitv-logo" className="inslog"></img>
+          <img src={inslogo} alt="iiitv-logo" className="inslog"></img>
         </div>
         <div className="iiitv-name">
           Indian Institute of Information Technology Vadodara
@@ -48,7 +47,7 @@ const Footer = () => {
               LINKS
               <hr className="underline"></hr>
             </div>
-            <div class="link-toogle" onClick = {() => link_toogle()} >
+            <div class="link-toogle" onClick={() => link_toogle()} >
               <i class="fa fa-angle-down fa-2x" aria-hidden="true" color="white"></i>
             </div>
           </div>
@@ -61,27 +60,27 @@ const Footer = () => {
         </div>
 
         <div className="footerlin">
-        <div className="conna">
-          CONTACT
+          <div className="conna">
+            CONTACT
           <hr className="underline"></hr>
-        </div>
-        <div className="contact">
-          <div className="clink">Email: <div className="con-details">cerebro@iiitvadodara.ac.in</div></div>
-          <div className="clink">Phone: <div className="con-details">+915525252536  +91242526252652  +9135252252255</div></div>
-          <div className="clink">Address: <div className="con-details">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div></div>
-        </div>
+          </div>
+          <div className="contact">
+            <div className="clink">Email: <div className="con-details">cerebro@iiitvadodara.ac.in</div></div>
+            <div className="clink">Phone: <div className="con-details">+915525252536  +91242526252652  +9135252252255</div></div>
+            <div className="clink">Address: <div className="con-details">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div></div>
+          </div>
         </div>
       </div>
+      
       <hr className="lastline"></hr>
-
-      <div className="dev"> 
+      <div className="dev">
         <div className="dot">
           <p className="p-dev">Designed by Dot</p>
-          <img src = {dotlogo} alt="dot-logo" className="dotlogo"></img>
+          <img src={dotlogo} alt="dot-logo" className="dotlogo"></img>
         </div>
         <div className="cc">
           <p className="p-dev">Developed by Coding Club</p>
-          <img src = {cclogo} alt="cc-logo" className="dotlogo"></img>
+          <img src={cclogo} alt="cc-logo" className="dotlogo"></img>
         </div>
       </div>
     </footer>
