dc4349f9634d0d9991db3d7703928036be728a13
sidharth saini <55971223+sidharth3000@users.noreply.github.com>
2021-01-13 02:50:07 +0530

    initializing mobileNav and sideDrawer (#32)
    Co-authored-by: Shivansh Kumar <56130670+ShivanshKumar@users.noreply.github.com>
    Co-authored-by: Jatin Gupta <56168174+jatin0814@users.noreply.github.com>
    Co-authored-by: Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
    Co-authored-by: Aman Raj <42104907+AmanRaj1608@users.noreply.github.com>

---
+++ src/Component/Navigation/Navigation.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import Header from '../Header';
+import SideDrawer from '../SideDrawer/SideDrawer';
+
+class Navigation extends Component {
+
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedhandler = () => {
+            this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerOpenedhandler = () => {
+        this.setState({showSideDrawer: true})
+}
+
+    render() {
+        return(
+            <div>
+                <Header opened={this.sideDrawerOpenedhandler }/>
+                <SideDrawer open ={this.state.showSideDrawer} closed={this.sideDrawerClosedhandler } />
+            </div>
+            
+        )
+    }
+}
+
+export default Navigation;
