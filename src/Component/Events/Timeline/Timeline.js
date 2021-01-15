71a46881b2bf94c18be463c680a2e57d2496fe7f
Shivansh <shivanshkumar693@gmail.com>
2021-01-16 01:58:03 +0530

    events-log.json import removed

--- src/Component/Events/Timeline/Timeline.js
+++ src/Component/Events/Timeline/Timeline.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import TimelineCard from '../TimelineCard/TimelineCard';
 import './Timeline.css';
-import events from './event-details.json';
 import Fade from 'react-reveal/Fade'
 class Timeline extends React.Component{
 
