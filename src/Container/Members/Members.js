b03d26a15f7a88e8c9e4d90c22dbadd54d0468e9
Aditya Prakash <p.aditya.198@gmail.com>
2021-01-31 13:47:32 +0530

    Made with Love :sparkles:

--- src/Container/Members/Members.js
+++ src/Container/Members/Members.js
@@ -1,63 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import Member from '../../Component/Members/Member/Member';
-import Navbar from '../../Component/Members/Navbar/Navbar';
-import './Members.css';
+import React, { useState, useEffect } from "react";
+import Member from "../../Component/Members/Member/Member";
+import Navbar from "../../Component/Members/Navbar/Navbar";
+import "./Members.css";
 import Footer from "../../Component/Footer";
 import Navigation from "../../Component/Navigation";
 
 const Members = () => {
+  const [teams, setTeams] = useState("");
+  const [currentTab, setCurrentTab] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
-	const [teams, setTeams] = useState("");
-	const [currentTab, setCurrentTab] = useState(0);
-	const [isLoading, setIsLoading] = useState(false);
+  const changeCurrentTab = (tab) => {
+    setCurrentTab(tab);
+  };
 
-	const changeCurrentTab = (tab) => {
-		setCurrentTab(tab);
-	}
+  useEffect(() => {
+    async function fetchAPI() {
+      setIsLoading(true);
+      const res = await fetch("https://kreiva.tk/team/");
+      const data = await res.json();
+      setTeams(data.Team);
+      setIsLoading(false);
+      /*------pre loading images------*/
+      data.Team.map((team) =>
+        team.teamMembers.map((member) => {
+          const img = new Image();
+          img.src = member.profilepic;
+          return img;
+        })
+      );
+    }
+    fetchAPI();
+    window.scroll(0, 0);
+  }, []);
 
-	useEffect(() => {
-		async function fetchAPI() {
-			setIsLoading(true);
-			const res = await fetch('https://kreiva.tk/team/');
-			const data = await res.json()
-			setTeams(data.Team);
-			setIsLoading(false);
-			console.log(data.Team);
-			/*------pre loading images------*/
- 			data.Team.map((team)=>team.teamMembers.map((member)=>{
-				const img = new Image();
-				img.src = member.profilepic;
-				return img;
-			}));
-		}
-		fetchAPI();
-		window.scroll(0,0);
-	}, [])
+  const teamNames = teams !== "" ? teams.map((team) => team.name) : null;
 
-	const teamNames = teams !== "" ? teams.map((team) => team.name) : null;
+  const CurrentTabMembers =
+    teams !== ""
+      ? teams[currentTab].teamMembers.map((member) => (
+          <Member
+            imgSrc={member.profilepic}
+            name={`${member.first_name} ${member.last_name}`}
+            position={member.role}
+            linkedIn={member.linkedIn}
+          />
+        ))
+      : [];
 
-	const CurrentTabMembers = teams !== "" ? teams[currentTab].teamMembers.map(
-		(member) => <Member imgSrc={member.profilepic}
-			name={`${member.first_name} ${member.last_name}`}
-			position={member.role}
-			linkedIn={member.linkedIn} />) : [];
-
-	return (
-		<React.Fragment>
-			<Navigation />
-			<Navbar teamNames={teamNames} currentTab={currentTab} changeCurrentTab={changeCurrentTab} />
-			<div className="members">
-				{
-					isLoading ?
-						<div className="lds-facebook">
-							<div></div><div></div><div></div>
-						</div>
-						: CurrentTabMembers
-				}
-			</div>
-			<Footer />
-		</React.Fragment>
-	)
-}
+  return (
+    <React.Fragment>
+      <Navigation />
+      <Navbar
+        teamNames={teamNames}
+        currentTab={currentTab}
+        changeCurrentTab={changeCurrentTab}
+      />
+      <div className="members">
+        {isLoading ? (
+          <div className="lds-facebook">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        ) : (
+          CurrentTabMembers
+        )}
+      </div>
+      <Footer />
+    </React.Fragment>
+  );
+};
 
 export default Members;
